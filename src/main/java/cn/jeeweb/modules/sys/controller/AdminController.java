7b7c4e4003b0cec8078df206c891addb302b794e
wangyu <wymyxlqm@163.com>
2018-11-26 10:41:00 +0800

    登录后的跳转问题修复

--- src/main/java/cn/jeeweb/modules/sys/controller/AdminController.java
+++ src/main/java/cn/jeeweb/modules/sys/controller/AdminController.java
@@ -1,7 +1,16 @@
 package cn.jeeweb.modules.sys.controller;
 
 import cn.jeeweb.core.common.controller.BaseController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
+@Controller
+@RequestMapping("/admin")
 public class AdminController extends BaseController {
 
+    @RequestMapping(value = "/")
+    public ModelAndView login() {
+        return new ModelAndView("redirect:"+"/rishiqing");
+    }
 }
